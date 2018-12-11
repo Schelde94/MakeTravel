7463237bc97dfbe269bce15e44931a67a1557be8
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-12-11 12:43:11 +0100

    added gradient to background

--- front-end/index.php
+++ front-end/index.php
@@ -6,7 +6,7 @@
 <title>Make Travel Companion</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">	
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Exo+2:300,400,500,700" rel="stylesheet">
 	
 <link rel="stylesheet" href="styles.css">	
 
@@ -20,7 +20,7 @@
 		</div>
 	</div>
 		
-	<div class="container-fluid bg-success">
+	<div class="container-fluid">
 		<div class="row justify-content-around">
 			<p class="lead text-center col-sm text-white mb-4 mt-4">All your files are inlcuded in the sections below.<br>Click on the sections to see vouchers and more detailed information.</p>
 		</div>
@@ -59,9 +59,9 @@
 		</div>
 	</div>
 	
-	<div class="container-fluid bg-dark mt-3 mb-5">
+	<div class="container-fluid mt-3 mb-5">
 		<div class="row justify-content-center">
-			<h1 class=""><strong>make</strong>travel</h1>
+			<h1 class="logoname"><strong>make</strong>travel</h1>
 		</div>
 		<div class="row justify-content-center">
 			<p class="lead">Tel. +298 232100</p>
@@ -99,9 +99,11 @@
 <!-- modal general -->		
 <?php include 'modalgeneral.php';?>
 <!-- modal general END -->			
-		
+
+	
 <button onclick="topFunction()" class="bg-dark rounded-0" id="myBtn" title="Go to top">Top</button>	
 
+	
 	
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
