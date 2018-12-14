6f20bdc3bb7845119f9b318d822b2c484b246fba
Schelde94 <s.schelde88@gmail.com>
2018-12-14 14:32:51 +0100

    Input name og form action

--- maketravelapp/index.php
+++ maketravelapp/index.php
@@ -13,11 +13,11 @@
 	<!-- header END -->
 	
 	<div class="container mb-5 mt-5">
-		<form>
+		<form action="home.php" method="post">
   			<div class="form-group row">
     			<label for="inputEmail3" class="col-sm-2 col-form-label text-white mt-5">Customer number</label>
     			<div class="col-sm-8">
-      			<input type="number" class="form-control mt-5 rounded-0" id="inputEmail3" placeholder="Write your customer number here!">
+      				<input name="kundenummer" type="number" class="form-control mt-5 rounded-0" id="inputEmail3" placeholder="Write your customer number here!">
     			</div>
 				<button type="submit" class="btn btn-dark col-sm-8 col-md mx-auto mt-5 rounded-0">Enter</button>
   			</div>
