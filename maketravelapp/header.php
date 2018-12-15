582b20e678398a4abbd399e37baff90de655e8a2
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-12-15 19:37:57 +0100

    Revert "Ny header test squash commit fra develop"
    This reverts commit 50f28ef776f1fbb41c8c9cb453513959b48dc979.

--- maketravelapp/header.php
+++ maketravelapp/header.php
@@ -2,6 +2,5 @@
 	<div class="row justify-content-center">
 		<img class="mb-4 mt-4 img-fluid d-none d-sm-block" src="img/logo.png" alt="Make Travel Logo">
 		<img class="mb-4 mt-4 img-fluid d-sm-none" src="img/logo-sm.png" alt="Make Travel Logo">
-		<h1>HEY WELCOME</h1>
 	</div>
 </div>
